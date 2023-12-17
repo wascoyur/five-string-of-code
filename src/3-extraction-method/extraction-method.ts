120d6c5980835ab5456b56f94d1978955aba23f7
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-12-17 20:33:47 +0500

    example Извлечение метода start

--- src/3-extraction-method/extraction-method.ts
+++ src/3-extraction-method/extraction-method.ts
@@ -1,10 +1,24 @@
 import * as path from "path";
 
-export const extractionMethod=()=>{
-    /*get method*/
+export const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-    const filename = path.basename(__filename);
-    console.log(`filename: ${filename}`)
+export const extractionMethod = () => {
+  /*get method*/
 
+  const filename = path.basename(__filename);
+  console.log(`filename: ${filename}`);
 
+  return { minimum };
+};
+
+function minimum(arr: number[][]) {
+  let result = Number.POSITIVE_INFINITY;
+  for (let x = 0; x < arr.length; x++)
+    for (let y = 0; y < arr[x].length; y++)
+      if (result > arr[x][y]) result = arr[x][y];
+  return result;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log(`Hellow-2`)
+import {
+  extractionMethod,
+  matrix,
+} from "./3-extraction-method/extraction-method";
+
+console.log(`main`);
+const min = extractionMethod().minimum(matrix);
+console.log(min);
