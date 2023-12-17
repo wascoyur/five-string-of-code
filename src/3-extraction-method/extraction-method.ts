61725466a537b31967f7b95d4cdefeee442c4f81
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-12-17 20:35:19 +0500

    example Извлечение метода finish

--- src/3-extraction-method/extraction-method.ts
+++ src/3-extraction-method/extraction-method.ts
@@ -18,7 +18,10 @@ export const extractionMethod = () => {
 function minimum(arr: number[][]) {
   let result = Number.POSITIVE_INFINITY;
   for (let x = 0; x < arr.length; x++)
-    for (let y = 0; y < arr[x].length; y++)
-      if (result > arr[x][y]) result = arr[x][y];
+    for (let y = 0; y < arr[x].length; y++) result = min(result, arr, x, y);
+  return result;
+}
+function min(result: number, arr: number[][], x: number, y: number) {
+  if (result > arr[x][y]) result = arr[x][y];
   return result;
 }
