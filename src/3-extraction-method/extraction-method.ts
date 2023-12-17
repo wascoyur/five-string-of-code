4ff39afe89af1f7d1017737610cac327df35cf26
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-12-17 19:30:48 +0500

    start

---
+++ src/3-extraction-method/extraction-method.ts
@@ -0,0 +1,10 @@
+import * as path from "path";
+
+export const extractionMethod=()=>{
+    /*get method*/
+
+    const filename = path.basename(__filename);
+    console.log(`filename: ${filename}`)
+
+
+}
